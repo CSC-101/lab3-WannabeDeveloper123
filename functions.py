f4c4d03da45e56b38f8dc535be2393de3b6d8554
WannabeDeveloper123 <vishal.murali.kannan@gmail.com>
2025-01-24 09:43:08 -0800

    lab 3 commit

--- functions.py
+++ functions.py
@@ -2,5 +2,8 @@
 # Input: a number to be doubled
 # Result: a number
 def double(n:int) -> int:
-    result = n * n
+    result = n * 2
     return result
+
+#answer to question listed in task 3: since it was n*n when we passed the first time,
+#it is wrong to say that the code was correct. 2 tests were required here to check whether the function follows its intended purpose.
